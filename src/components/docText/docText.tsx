29dcf162e470ba979d82d779c93f3e67fa94b881
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2023-06-18 16:16:40 +0800

    Add files via upload

---
+++ src/components/docText/docText.tsx
@@ -0,0 +1,7 @@
+import './docText.less'
+export default function DocText(){
+    return <div className="doc-text">
+        <h2>What is a Convolutional Neural Network?</h2>
+        <p>In machine learning, a classifier assigns a class label to a data point. For example, an image classifier produces a class label (e.g, bird, plane) for what objects exist within an image. A convolutional neural network, or CNN for short, is a type of classifier, which excels at solving this problem!</p>
+    </div>
+}
