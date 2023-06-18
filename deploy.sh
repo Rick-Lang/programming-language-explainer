91d7078dd003f6813b9a86d7f55bd33a4a22eede
763943829@qq.com <763943829@qq.com>
2023-06-18 16:44:45 +0800

    fix: build bug

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 拷贝REMADE.md文件到dist李
+cp README.md dist
+
+# 进入生成的文件夹
+cd dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m '双哥哥又在辛苦的学习更新代码了'
+
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f https://rick-lang.github.io/programming-language-explainer.git gh-pages
+
+cd -
