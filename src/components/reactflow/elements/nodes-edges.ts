b95f279e3e022db2352cd0b286795e57b377856f
Satin Wuker <flyingsatin@gmail.com>
2023-08-13 17:57:22 -0700

    Fix display bugs

--- src/components/reactflow/elements/nodes-edges.ts
+++ src/components/reactflow/elements/nodes-edges.ts
@@ -24,16 +24,16 @@ export const initialNodes = [
     position: { x: 0, y: 200 },
   },
   {
-    id: 'Intereter',
-    data: { label: 'interpreter' },
+    id: 'Interpreter',
+    data: { label: 'Interpreter' },
     type: 'custom',
-    position: { x: 250, y: 100 },
+    position: { x: 0, y: 300 },
   },
   {
     id: 'python',
-    data: { label: 'transpiler: python' },
+    data: { label: 'Transpiler: Python' },
     type: 'custom',
-    position: { x: 500, y: 100 },
+    position: { x: 250, y: 100 },
   },
 
 ].map((node) => {
@@ -43,16 +43,16 @@ export const initialNodes = [
 export const initialEdges = [
   { id: 'e12', source: 'Source', target: 'Lexer', type: edgeType, animated: true, },
   { id: 'e2Parser', source: 'Lexer', target: 'Parser', type: edgeType, animated: true },
-  { id: 'e2Parser1', source: 'Lexer', target: 'Intereter', type: edgeType, animated: true },
-  { id: 'e2transpiler', source: 'Intereter', target: 'python', type: edgeType, animated: true },
+  { id: 'e2transpiler', source: 'Lexer', target: 'python', type: edgeType, animated: true },
+  { id: 'e2Parser2', source: 'Parser', target: 'Interpreter', type: edgeType, animated: true },
 
 ].map((edge) => {
   return {
     ...edge,
     markerEnd: {
       type: MarkerType.ArrowClosed,
-      width: 20,
-      height: 20,
+      width: 30,
+      height: 30,
     },
   }
 })
