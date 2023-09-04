8f156f29d24442cb33d05560f5817ecd8304a46a
763943829@qq.com <763943829@qq.com>
2023-09-05 01:18:36 +0800

    feat: add theme check

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import {
   useScroll,
   useSpring,
 } from "framer-motion";
-import "github-markdown-css/github-markdown-dark.css"
+import "github-markdown-css/github-markdown.css"
 import "cornercss/index.css";
+import { Switch } from 'antd';
+import { useState } from 'react';
 function App() {
   const { scrollYProgress } = useScroll();
   const scaleX = useSpring(scrollYProgress, {
@@ -15,11 +17,21 @@ function App() {
     damping: 30,
     restDelta: 0.001
   });
+  const [theme, setTheme] = useState(true)
+  const onChange = (e: boolean) => {
+    setTheme(e)
+  }
   return (
-    <div className='markdown-body' >
+    <div className={`markdown-body ${theme ? '' : 'dark-theme'}`} >
       <motion.div className="progress" style={{ scaleX }} />
       <div className='header'>
         <div>Programming Language Explainer - By Rick Lang</div>
+        <div className='header-right'>
+          <Switch
+            onChange={onChange}
+            defaultChecked
+          />
+        </div>
       </div>
       <div className='content' style={{ height: 'calc(100vh - 100px)', width: '' }}>
         <ReactFlowProvider ></ReactFlowProvider>
